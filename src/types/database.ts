9fb8aac1c60a8ef727db631be2822d5a7e12fe42
TroExol <zeidxol@gmail.com>
2025-11-25 17:54:47 +0500

    remove public.users

--- src/types/database.ts
+++ src/types/database.ts
@@ -1,26 +1,6 @@
 export interface Database {
   public: {
     Tables: {
-      users: {
-        Row: {
-          id: string;
-          email: string;
-          created_at: string;
-          updated_at: string;
-        };
-        Insert: {
-          id?: string;
-          email: string;
-          created_at?: string;
-          updated_at?: string;
-        };
-        Update: {
-          id?: string;
-          email?: string;
-          created_at?: string;
-          updated_at?: string;
-        };
-      };
       preferences: {
         Row: {
           id: string;
